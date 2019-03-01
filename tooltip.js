09268539d2063a2a6f339471773bf1d79315a84d
Rui Baiao <rui.baiao@tingcore.com>
2019-03-01 10:35:32 +0100

    Custom components styling and shadowDOM

--- tooltip.js
+++ tooltip.js
@@ -3,27 +3,66 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = 'Some dummy tooltip text.';
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        div {
+          background-color: black;
+          padding: 5px 5px 5px 5px;
+          color: white;
+          position: absolute;
+          z-index: 10;
+        }
+
+        :host {
+          background: lightgrey;
+        }
+
+        .highlight {
+          background-color: red;
+        }
+        ::slotted(.highlight) {
+          border-bottom: 1px dotted red;
+        }
+
+        .icon {
+          background: black;
+          color: white;
+          padding: 0.2rem 0.6rem;
+          tedxt-align: center;
+          cursor: pointer;
+          border-radius: 50%;
+        }
+      </style>
+      <slot>Default content</slot>
+      <span class="icon">?</span>
+    `;
   }
   
   connectedCallback() {
     if(this.hasAttribute('text')) {
-      this._tooltipText = this.getAttribute('text')
+      this._tooltipText = this.getAttribute('text');
     }
-    const tooltipIcon = document.createElement('span');
-    tooltipIcon.textContent = ' (?)';
+    const tooltipIcon = this.shadowRoot.querySelector('span');
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    this.appendChild(tooltipIcon);
+    this.shadowRoot.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
-    this.appendChild(this._tooltipContainer);
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.padding = '5px 5px 5px 5px';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this._tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
